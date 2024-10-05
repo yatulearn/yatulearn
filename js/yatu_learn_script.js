ae096932558a48c3cecc65cc6801fb5dcb2e0cd5
YATU Learn <yatulearn@gmail.com>
2024-10-05 18:33:13 +0530

    Update yatu_learn_script.js

--- js/yatu_learn_script.js
+++ js/yatu_learn_script.js
@@ -1,15 +1,15 @@
-var coll = document.getElementsByClassName("collapsible");
-for (var i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
-    });
-}
+// var coll = document.getElementsByClassName("collapsible");
+// for (var i = 0; i < coll.length; i++) {
+//     coll[i].addEventListener("click", function() {
+//         this.classList.toggle("active");
+//         var content = this.nextElementSibling;
+//         if (content.style.display === "block") {
+//             content.style.display = "none";
+//         } else {
+//             content.style.display = "block";
+//         }
+//     });
+// }
 let nav=document.querySelector("nav");
 let btnh=document.querySelector(".btn-home");
 let btnq=document.querySelector(".qp");
