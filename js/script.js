a28b907ade358548dfb6eb926526986a7054d024
Devanshu Kumar Singh <console.uk123@gmail.com>
2024-10-08 14:08:55 +0530

    solved issue #211

--- js/script.js
+++ js/script.js
@@ -40,3 +40,21 @@ scrollUpLinks.forEach(id => {
       link.addEventListener('click', scrollToTop);
   }
 });
+
+const scrollUpBtn = document.getElementById('scrollUpBtn');
+
+scrollUpBtn.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 200) {
+    scrollUpBtn.style.display = 'block';
+  } else {
+    scrollUpBtn.style.display = 'none';
+  }
+});
+
+scrollUpBtn.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
